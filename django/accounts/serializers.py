be9938e9bd5469f391b40ed101a9e6c40128fe8a
Gunwoo Kim <gunwooterry@gmail.com>
2018-04-06 01:15:48 +0900

    feat: implement user model and API

---
+++ django/accounts/serializers.py
@@ -0,0 +1,28 @@
+from django_countries.serializers import CountryFieldMixin
+from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
+from sorl_thumbnail_serializer.fields import HyperlinkedSorlImageField
+
+from .models import User
+
+
+class UserSerializer(CountryFieldMixin, ModelSerializer):
+    level = serializers.IntegerField(source='get_level')
+    profile_thumbnail = HyperlinkedSorlImageField(
+        '128x128',
+        source='profile_image',
+        options={'crop': 'center'},
+        read_only=True,
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'name',
+            'nationality',
+            'profile_thumbnail',
+            'preferred_language',
+            'is_expert',
+            'level',
+        )
--- django/glossm/urls.py
+++ django/glossm/urls.py
@@ -1,7 +1,14 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from accounts.views import Profile
+
 urlpatterns = [
     path('manage/', admin.site.urls),
+    path('profile/', Profile.as_view()),
     path('transcription/', include('transcription.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
