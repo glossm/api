1057d4e2d6d3a4354925f1c4878a965b59546aa1
Gunwoo Kim <gunwooterry@gmail.com>
2018-04-13 18:07:21 +0900

    feat: track proficiency of each language separately

--- django/transcription/serializers.py
+++ django/transcription/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, ValidationError
 
 from core.models import Record
 from .models import Submission
@@ -20,13 +20,17 @@ class SubmissionSerializer(ModelSerializer):
 
     def create(self, validated_data):
         user = self.context['request'].user
+        record = validated_data['record']
         submission = Submission.objects.create(
             submitter=user,
-            record=validated_data['record'],
+            record=record,
             answer=validated_data['answer'],
         )
-        user.exp += submission.score()
-        user.save()
+        proficiency = user.proficiency.filter(language=record.language).first()
+        if proficiency is None:
+            raise ValidationError('You are not learning this language.')
+        proficiency.exp += submission.score()
+        proficiency.save()
         return submission
 
     def get_top_answers(self, submission):
