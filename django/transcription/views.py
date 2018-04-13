5c614f0a1c4048d5ac56a7d9c6323996d06ed207
Gunwoo Kim <gunwooterry@gmail.com>
2018-04-13 21:20:38 +0900

    feat: track learning status

--- django/transcription/views.py
+++ django/transcription/views.py
@@ -1,9 +1,16 @@
 from rest_framework.generics import CreateAPIView
 
 from .models import Submission
-from .serializers import SubmissionSerializer
+from .serializers import SubmissionSerializer, ProficiencySerializer
 
 
 class SubmissionCreate(CreateAPIView):
     serializer_class = SubmissionSerializer
     queryset = Submission.objects.all()
+
+
+class ProficiencyCreate(CreateAPIView):
+    serializer_class = ProficiencySerializer
+
+    def get_queryset(self):
+        return self.request.user.proficiency
