a37e48461b1c6c9b2b3d9fecc0ebd340e2f1cd93
Gunwoo Kim <gunwooterry@gmail.com>
2018-05-24 12:54:14 +0900

    feat: add language detail view

--- django/core/urls.py
+++ django/core/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TopicList, LanguageList, RecordList
+from .views import TopicList, LanguageList, LanguageDetail, RecordList
 
 urlpatterns = [
     path('languages/', LanguageList.as_view()),
-    path('languages/<str:code>/topics/', TopicList.as_view()),
-    path('languages/<str:code>/topics/<int:topic_id>/records', RecordList.as_view()),
+    path('languages/<int:id>/', LanguageDetail.as_view()),
+    path('languages/<int:id>/topics/', TopicList.as_view()),
+    path('languages/<int:lang_id>/topics/<int:topic_id>/records/', RecordList.as_view()),
 ]
