ec9ebf727775e6a756471e96dffabe018cf6914c
Gunwoo Kim <gunwooterry@gmail.com>
2018-05-24 12:54:14 +0900

    feat: add language detail view

--- django/core/views.py
+++ django/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.exceptions import NotFound
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from .models import Language
 from .serializers import TopicSerializer, LanguageSerializer, RecordSerializer
@@ -10,7 +10,7 @@ class TopicList(ListAPIView):
     serializer_class = TopicSerializer
 
     def get_queryset(self):
-        language = get_object_or_404(Language, code=self.kwargs['code'])
+        language = get_object_or_404(Language, id=self.kwargs['id'])
         topics = language.topic_set.topics
         return topics.order_by('level', 'name')
 
@@ -20,11 +20,17 @@ class LanguageList(ListAPIView):
     queryset = Language.objects.all()
 
 
+class LanguageDetail(RetrieveAPIView):
+    serializer_class = LanguageSerializer
+    queryset = Language.objects.all()
+    lookup_field = 'id'
+
+
 class RecordList(ListAPIView):
     serializer_class = RecordSerializer
 
     def get_queryset(self):
-        language = get_object_or_404(Language, code=self.kwargs['code'])
+        language = get_object_or_404(Language, id=self.kwargs['lang_id'])
         topic_id = self.kwargs['topic_id']
         topic = language.topic_set.topics.filter(id=topic_id).first()
         if topic is None:
