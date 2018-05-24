040cedbf33c42dd24aad2d666c8319af17978529
Gunwoo Kim <gunwooterry@gmail.com>
2018-05-24 17:03:43 +0900

    feat: upload files to AWS S3

--- django/glossm/settings.py
+++ django/glossm/settings.py
@@ -35,6 +35,7 @@ PREREQ_APPS = [
     'sorl_thumbnail_serializer',
     'rest_auth',
     'corsheaders',
+    'storages',
 ]
 
 PROJECT_APPS = [
@@ -128,6 +129,20 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+
+# AWS S3
+
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = 'glossm'
+AWS_DEFAULT_ACL = 'private'
+AWS_S3_REGION_NAME = 'ap-northeast-2'
+
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
