1563f230fad0402c1eb9dbfc1b90f478fbf230e1
Gunwoo Kim <gunwooterry@gmail.com>
2018-05-31 00:42:37 +0900

    feat: redirect root URL to admin
    Resolve #28.

--- django/glossm/urls.py
+++ django/glossm/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic.base import RedirectView
 
 from rest_framework_jwt.views import refresh_jwt_token, verify_jwt_token
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='manage/')),
     path('manage/', admin.site.urls),
     path('accounts/', include('rest_auth.urls')),
     path('accounts/registration/', include('rest_auth.registration.urls')),
