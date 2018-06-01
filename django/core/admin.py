606b89249c0a173c16345d210bce93c4db05e9c9
Gunwoo Kim <gunwooterry@gmail.com>
2018-06-01 15:05:38 +0900

    feat: implement CSV import/export (#31)
    Resolve #27.

--- django/core/admin.py
+++ django/core/admin.py
@@ -1,16 +1,45 @@
 from django.contrib import admin
 from django.contrib.admin import ModelAdmin
+from import_export.admin import ImportExportModelAdmin
+from import_export.formats import base_formats
+from import_export.resources import ModelResource
 
 from .models import TopicSet, Topic, Language, Meaning, Record
 
 
+class CustomIEModelAdmin(ImportExportModelAdmin):
+    def get_import_formats(self):
+        formats = (base_formats.CSV,)
+        return [f for f in formats if f().can_import()]
+
+    def get_export_formats(self):
+        formats = (base_formats.CSV,)
+        return [f for f in formats if f().can_export()]
+
+
+class TopicSetResource(ModelResource):
+    class Meta:
+        model = TopicSet
+        exclude = ('id',)
+        import_id_fields = ('name',)
+
+
 @admin.register(TopicSet)
-class TopicSetAdmin(ModelAdmin):
+class TopicSetAdmin(CustomIEModelAdmin):
+    resource_class = TopicSetResource
     list_display = ('name',)
 
 
+class TopicResource(ModelResource):
+    class Meta:
+        model = Topic
+        exclude = ('id',)
+        import_id_fields = ('name',)
+
+
 @admin.register(Topic)
-class TopicAdmin(ModelAdmin):
+class TopicAdmin(CustomIEModelAdmin):
+    resource_class = TopicResource
     list_display = (
         'name',
         'level',
@@ -18,8 +47,16 @@ class TopicAdmin(ModelAdmin):
     )
 
 
+class LanguageResource(ModelResource):
+    class Meta:
+        model = Language
+        exclude = ('id',)
+        import_id_fields = ('code',)
+
+
 @admin.register(Language)
-class LanguageAdmin(ModelAdmin):
+class LanguageAdmin(CustomIEModelAdmin):
+    resource_class = LanguageResource
     list_display = (
         'name',
         'code',
@@ -27,8 +64,16 @@ class LanguageAdmin(ModelAdmin):
     )
 
 
+class MeaningResource(ModelResource):
+    class Meta:
+        model = Meaning
+        exclude = ('id',)
+        import_id_fields = ('ask_code',)
+
+
 @admin.register(Meaning)
-class MeaningAdmin(ModelAdmin):
+class MeaningAdmin(CustomIEModelAdmin):
+    resource_class = MeaningResource
     list_display = (
         'ask_code',
         'en',
